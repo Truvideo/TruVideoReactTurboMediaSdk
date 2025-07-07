7eb389f4038dc0a7c2f0157e5773a5d1773a01ea
Anurag <anurag.singh@5exceptions.com>
2025-07-07 21:21:02 +0530

    update

--- ios/TruvideoReactTurboMedia.swift
+++ ios/TruvideoReactTurboMedia.swift
@@ -6,7 +6,7 @@ import React
 
 @objc public class TruVideoReactMediaSdkClass: NSObject {
     private var disposeBag = Set<AnyCancellable>()
-    
+
     @objc public func mediaBuilder(filePath: String, tag: String, metaData: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
         guard let fileURL = URL(string: "file://\(filePath)") else {
             reject("INVALID_URL", "The file URL is invalid", nil)
@@ -16,7 +16,7 @@ import React
         do {
             let builder = try createFileUploadRequestBuilder(fileURL: fileURL, tag: tag, metaData: metaData)
           var request = try builder.build()
-          
+
           let mainResponse: [String: String] = [
             "id": request.id.uuidString, // Generate a unique ID for the event
             "filePath": request.filePath,
@@ -43,16 +43,16 @@ import React
             reject("UPLOAD_ERROR", "Upload failed", error)
         }
     }
-    
+
     private func createFileUploadRequestBuilder(fileURL: URL, tag: String, metaData: String) throws -> TruvideoSdkMedia.FileUploadRequestBuilder {
         let builder = TruvideoSdkMedia.FileUploadRequestBuilder(fileURL: fileURL)
-        
+
         // Convert tag JSON string to dictionary
         let tagDict = try convertToDictionary(from: tag)
         for (key, value) in tagDict {
             builder.addTag(key, "\(value)")
         }
-      
+
         // Convert metadata JSON string to Metadata type
         let metadataObj = try convertToDictionary(from: metaData)
         for (key, value) in metadataObj {
@@ -60,13 +60,13 @@ import React
         }
         return builder
     }
-    
+
     private func executeUploadRequest(builder: TruvideoSdkMedia.FileUploadRequestBuilder, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) throws {
         let request =  try builder.build()
-        
+
         // Print the file upload request for debugging
         print("fileUploadRequest: ", request.id.uuidString)
-        
+
         // Completion of request
         let completeCancellable = request.completionHandler
             .receive(on: DispatchQueue.main)
@@ -78,6 +78,7 @@ import React
                     // Print any errors that occur during the upload process
                     print("Upload failure:", error)
                     reject("UPLOAD_ERROR", "Upload failed", error)
+                  self.sendEvent(withName: "onError", body: error.localizedDescription)
                 }
             }, receiveValue: { uploadedResult in
                 // Upon successful upload, retrieve the uploaded file URL
@@ -88,7 +89,7 @@ import React
                 let transcriptionLength = uploadedResult.transcriptionLength
                 let id = request.id.uuidString
               print("uploadedResult: ", uploadedResult)
-                
+
                 print("tags: " , tags.dictionary)
                 print("metaData: " , metadataDict.dictionary)
                 // Send completion event
@@ -100,15 +101,23 @@ import React
                     "transcriptionURL": transcriptionURL,
                     "transcriptionLength": transcriptionLength
                 ]
-                
+
                 // resolve
                 resolve(["status": mainResponse])
-              //self.sendEvent(withName: "onComplete", body: mainResponse)
+                do {
+                  let jsonData = try JSONSerialization.data(withJSONObject: mainResponse, options: [])
+
+                  if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    self.sendEvent(withName: "onComplete", body: jsonString)
+                  }
+                }catch{
+                  
+                }
             })
-        
+
         // Store the completion handler in the dispose bag to avoid premature deallocation
         completeCancellable.store(in: &disposeBag)
-        
+
         // Progress of request
         let progress = request.progressHandler
             .receive(on: DispatchQueue.main)
@@ -117,15 +126,23 @@ import React
                     "id": UUID().uuidString, // Generate a unique ID for the event
                     "progress": String(format: " %.2f %", progress.percentage * 100)
                 ]
-                //self.sendEvent(withName: "onProgress", body: mainResponse)
+              do {
+                let jsonData = try JSONSerialization.data(withJSONObject: mainResponse, options: [])
+
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                  self.sendEvent(withName: "onProgress", body: jsonString)
+                }
+              }catch{
+                
+              }
             })
-        
+
         // Store the progress handler in the dispose bag to avoid premature deallocation
         progress.store(in: &disposeBag)
-        
+
         try request.upload()
     }
-  
+
   @objc public func getFileUploadRequestById(id: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock){
     do {
       let request =  try TruvideoSdkMedia.getFileUploadRequest(withId : id)
@@ -154,7 +171,7 @@ import React
     } catch {
         resolve("{}")
     }
-    
+
     //TruvideoSdkMedia.FileUploadRequestBuilder(fileURL: fileURL)
   }
 
@@ -163,27 +180,27 @@ import React
     try? request?.cancel()
     resolve("Cancel Success")
   }
-  
+
   @objc public func deleteMedia(id: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock){
     let request = try? TruvideoSdkMedia.getFileUploadRequest(withId : id)
     try? request?.delete()
     resolve("Delete Success")
   }
-  
+
   @objc public func pauseMedia(id: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock){
     let request = try? TruvideoSdkMedia.getFileUploadRequest(withId : id)
     try? request?.pause()
     resolve("Pause Success")
   }
-  
+
   @objc public func resumeMedia(id: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock){
     let request = try? TruvideoSdkMedia.getFileUploadRequest(withId : id)
     try? request?.resume()
     resolve("Resume Success")
   }
-  
+
   @objc public func search(tag: String,type : String,page : String,pageSize : String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock){
-    
+
     let tagDict = try? convertToDictionary(from: tag)
     var tagBuild = TruvideoSdkMediaTags.builder()
     for (key, value) in tagDict! {
@@ -223,21 +240,21 @@ import React
         }else{
           reject("ERROR","JSON_ERROR",nil)
         }
-        
+
       }
-      
-      
+
+
     }
     //try? request?.resume()
   }
-  
-  
+
+
   @objc public func uploadMedia(id: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock){
     let request = try? TruvideoSdkMedia.getFileUploadRequest(withId : id)
-    
+
     // Print the file upload request for debugging
     //print("fileUploadRequest: ", request.id.uuidString)
-    
+
     // Completion of request
     let completeCancellable = request?.completionHandler
         .receive(on: DispatchQueue.main)
@@ -259,13 +276,13 @@ import React
             let transcriptionLength = uploadedResult.transcriptionLength
             let id = request?.id.uuidString
           print("uploadedResult: ", uploadedResult)
-            
+
             print("tags: " , tags.dictionary)
             print("metaData: " , metadataDict.dictionary)
             // Send completion event
             let dateFormatter = ISO8601DateFormatter()
-            
-          
+
+
           do {
             let tagJsonData = try JSONSerialization.data(withJSONObject: tags.dictionary, options: [])
             if let tagJsonString = String(data: tagJsonData, encoding: .utf8) {
@@ -285,10 +302,10 @@ import React
                 if let jsonString = String(data: jsonData, encoding: .utf8) {
                         print("mainResponse as JSON string: \(jsonString)")
                         resolve(jsonString) // Or wherever you need to use this JSON string
-    
+
                   self.sendEvent(withName: "onComplete", body: jsonString)
                     } else {
-                      
+
                         print("Error: Could not convert JSON data to string.")
                       reject("INVALID_JSON", "Error: Could not convert JSON data to string", nil)
                         // Handle error: e.g., reject(error)
@@ -296,16 +313,16 @@ import React
             }else {
               reject("INVALID_JSON", "Error: Could not convert JSON data to string", nil)
             }
-            
+
           }catch{
             reject("INVALID_JSON", "Error: Could not convert JSON data to string", nil)
           }
-            
+
         })
-    
+
     // Store the completion handler in the dispose bag to avoid premature deallocation
     completeCancellable?.store(in: &disposeBag)
-    
+
     // Progress of request
     let progress = request?.progressHandler
         .receive(on: DispatchQueue.main)
@@ -325,13 +342,13 @@ import React
             self.sendEvent(withName: "onProgress", body: "Unable to Parse JSON")
           }
         })
-    
+
     // Store the progress handler in the dispose bag to avoid premature deallocation
     progress?.store(in: &disposeBag)
-    
+
     try? request?.upload()
   }
-    
+
     private func convertToDictionary(from jsonString: String) throws -> [String: Any] {
         guard let jsonData = jsonString.data(using: .utf8) else {
             throw NSError(domain: "Invalid JSON string", code: 0, userInfo: nil)
@@ -346,15 +363,15 @@ import React
 
   private func convertToJsonString(from dictionary: [String: Any]) throws -> String {
       let jsonData = try JSONSerialization.data(withJSONObject: dictionary, options: [])
-      
+
       guard let jsonString = String(data: jsonData, encoding: .utf8) else {
           throw NSError(domain: "Unable to encode JSON string", code: 2, userInfo: nil)
       }
-      
+
       return jsonString
   }
 
-    
+
 //    private func convertToMetadata(from jsonString: String) throws -> Metadata {
 //        guard let jsonData = jsonString.data(using: .utf8) else {
 //            throw NSError(domain: "Invalid JSON string", code: 0, userInfo: nil)
@@ -416,7 +433,7 @@ import React
 //            return convertMetadataToDictionary(dictValue)
 //        }
   //  }
-    
+
     // Function to send events to React Native
     private func sendEvent(withName name: String, body: String) {
         guard let bridge = RCTBridge.current() else { return }
